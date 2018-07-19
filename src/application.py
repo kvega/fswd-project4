ab733888b51a14eef05e3d97929c49fb30d81bcc
Kevin Vega <kevin.n.vega@gmail.com>
2018-07-19 01:47:39 +0000

    Moved login layout to its own page

--- src/application.py
+++ src/application.py
@@ -39,7 +39,7 @@ session = DBSession()
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
     login_session['state'] = state
-    return "The current session state is %s" % login_session['state']
+    return render_template("login.html", STATE=state)
 
 
 # Connect to Google Login
