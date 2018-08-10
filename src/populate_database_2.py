d9bc254232620be1f44381ea4c7b5b8f9893d978
Kevin Vega <kevin.n.vega@gmail.com>
2018-08-10 05:55:17 +0000

    Added users to items in db

--- src/populate_database_2.py
+++ src/populate_database_2.py
@@ -31,11 +31,11 @@ CATEGORIES = [
 
 # List of items
 ITEMS = [
-        Item(category_id=1, title="Soccer Cleats", description="The shoes"),
-        Item(category_id=1, title="Jersey", description="The shirt"),
-        Item(category_id=2, title="Jersey", description="The shirt"),
-        Item(category_id=3, title="Bat", description="The bat"),
-        Item(category_id=5, title="Snowboard", description="The board")
+        Item(category_id=1, title="Soccer Cleats", description="The shoes", user_id=1),
+        Item(category_id=1, title="Jersey", description="The shirt", user_id=1),
+        Item(category_id=2, title="Jersey", description="The shirt", user_id=1),
+        Item(category_id=3, title="Bat", description="The bat", user_id=2),
+        Item(category_id=5, title="Snowboard", description="The board", user_id=2)
         ]
 
 # Add Categories to DB
@@ -59,4 +59,4 @@ def add_items(items):
 
 
 add_categories(CATEGORIES)
-# add_items(ITEMS)
+add_items(ITEMS)
