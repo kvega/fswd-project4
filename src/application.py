4307db3d6a329d3b28ab611d58045294d1b19efa
Kevin Vega <kevin.n.vega@gmail.com>
2018-08-13 00:07:14 +0000

    Fixed gdisconnect output

--- src/application.py
+++ src/application.py
@@ -179,10 +179,11 @@ def gdisconnect():
         response = make_response(json.dumps("Current user not connected"), 401)
         response.headers['Content-Type'] = "application/json"
         return response
-    print "In gdisconnect access token is %s", access_token
+    print "In gdisconnect access token is %s" % access_token
     print "User name is: " 
     print login_session["username"]
-    url = "https://accounts.google.com/o/oauth2/revoke?token=%s" % login_session["access_token"]
+    url = "https://accounts.google.com/o/oauth2/revoke?token=%s" % access_token
+    print url
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
     print "result is " 
